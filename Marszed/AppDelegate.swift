82a41468eecec8bdc4c69d73f6c5f57a12b72e03
py <marszed.pen@cn.ipx.net>
2017-08-08 14:22:40 +0800

    实现简单计算器

--- Marszed/AppDelegate.swift
+++ Marszed/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+// 当IOS系统与应用程序发生“交互”时，会调用AppDelegate
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,29 +17,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // 应用程序开始运行时调用
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // 当应用程序即将从活动状态移动到非活动状态时发送。对于某些类型的临时中断（例如传入的电话或SMS消息），或者当用户退出应用程序并开始向后台状态转换时，这种情况可能发生。b
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        //用这种方法来暂停正在进行的任务，关闭定时器，并使图形渲染回调。游戏应该使用这种方法来暂停游戏。
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // 使用此方法释放共享资源、保存用户数据、取消计时器，并存储足够的应用程序状态信息，以恢复应用程序的当前状态，以防止其稍后被终止。
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        // 如果您的应用程序支持后台执行，这种方法被称为替代applicationWillTerminate:当用户退出, 例如用户点击Home键。
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        // 作为从背景到活动状态的转换的一部分调用，在这里您可以撤消在进入后台时所做的许多更改。
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // 在应用程序处于非活动状态时重新启动暂停（或尚未启动）的任务。如果应用程序以前处于后台，可以选择刷新用户界面。
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // 应用程序即将终止时调用。如果需要保存数据, 见applicationDidEnterBackground
     }
 
 
--- Marszed/ViewController.swift
+++ Marszed/ViewController.swift
@@ -10,16 +10,122 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelResult: UILabel!
+    
+    var firstOperand: Double = 0.0; // 第一操作数
+    var secondOperand: Double = 0.0; // 第二操作数
+    var decimalPointFlag: Bool = false; // 标记是否输入了小数点
+    var isSecond: Bool = false; // 是否输入第二操作数
+    var operatorFlag: String = "" // 操作符
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 加载视图后再做任何额外设置，通常是从笔尖。
+        // self.view.backgroundColor = UIColor.yellow;
+        // let label = UILabel(frame: CGRect(x: 10, y: 170, width: 300, height: 50));
+        // label.text = "Marszed"
+        // label.textColor = UIColor.blue;
+        // self.view.addSubview(label);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // 处理任何可以重新创建的资源
     }
 
-
+    // 数字键 outLet Action
+    @IBAction func buttonTap(_ sender: UIButton) {
+        // labelResult 默认是0，如果开始输入数字，则先清除0
+        if (labelResult.text == "0" || (isSecond && secondOperand == 0.0)){
+            labelResult.text = "";
+        }
+        
+        // 将用户输入的数添加到labelResult
+        labelResult.text = labelResult.text! + sender.titleLabel!.text!;
+        
+        if isSecond{
+            secondOperand = NSString(string:labelResult.text!).doubleValue;
+        } else {
+            firstOperand = NSString(string:labelResult.text!).doubleValue;
+        }
+    
+        print(firstOperand);
+    }
+    
+    // 小数点 outLet Action
+    @IBAction func decimalPointTag() {
+        // 如果没有输入小数点则执行下面操作
+        if !decimalPointFlag{
+            secondOperand = NSString(string:labelResult.text!).doubleValue;
+        } else {
+            firstOperand = NSString(string:labelResult.text!).doubleValue;
+        }
+        decimalPointFlag = !decimalPointFlag;
+    }
+    
+    // 运算符
+    @IBAction func operatorTag(_ sender: UIButton) {
+        if firstOperand != 0 {
+            isSecond = true;
+            decimalPointFlag = false;
+            
+            switch sender.titleLabel!.text! {
+            case "+":
+                operatorFlag = "+";
+            case "-":
+                operatorFlag = "-";
+            case "×":
+                operatorFlag = "*";
+            case "÷":
+                operatorFlag = "/";
+            default:
+                operatorFlag = "";
+            }
+        }
+    }
+    
+    // 计算
+    @IBAction func resultTap() {
+        // 确保第二操作数有值
+        if isSecond {
+            // 除数不能为0
+            if operatorFlag == "/" && secondOperand == 0 {
+                print("Error: 除数不能为0");
+                return;
+            }
+            
+            var result: Double = 0.0;
+            switch operatorFlag {
+            case "+":
+                result = firstOperand + secondOperand;
+            case "-":
+                result = firstOperand - secondOperand;
+            case "*":
+                result = firstOperand * secondOperand;
+            case "/":
+                result = firstOperand / secondOperand;
+            default:
+                result = 0.0;
+            }
+            
+            labelResult.text = result.description;
+            print(" 第一操作数: \(firstOperand)");
+            print(" 操作符: \(operatorFlag)");
+            print(" 第二操作数: \(secondOperand)");
+            print(" 结果: \(result)");
+        }
+    }
+    
+    // 归0，清空
+    @IBAction func clear() {
+        labelResult.text = "0";
+        firstOperand = 0.0;
+        secondOperand = 0.0;
+        decimalPointFlag = false;
+        isSecond = false;
+        operatorFlag = "";
+    }
 }
 
